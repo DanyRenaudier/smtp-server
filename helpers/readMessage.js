17620790324aed4e0cc9b61c8322229bc054a801
Daniel Renuaider <90731476+DanyRenaudier@users.noreply.github.com>
2024-08-19 17:57:27 +0000

    grammatical errors fixed

--- helpers/readMessage.js
+++ helpers/readMessage.js
@@ -2,7 +2,7 @@ const { readFileSync } = require('node:fs');
 
 const Message = async () => new Promise((resolve, reject) => {
     let message = readFileSync('./Message.txt', 'utf-8')
-    message ? resolve(message) : reject('No message defined to sent')
+    message ? resolve(message) : reject('No message defined to be sent')
 });
 
 module.exports= {
